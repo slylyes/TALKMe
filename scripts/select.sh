66be468c5d50d979ffbed635932b35cf6532a523
slylyes <lyes008007@gmail.com>
2025-05-21 18:09:18 +0200

    changement d'emplacement des scripts

---
+++ scripts/select.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+curl --noproxy localhost --location 'http://localhost:8080/data/filter' \
+--header 'Content-Type: application/json' \
+--data '{
+    "name": "Table",
+    "columns": [
+        "vendor_name",
+        "Passenger_Count",
+        "Total_Amt",
+        "Trip_Distance"
+    ],
+    "filters": [
+        ["vendor_name","=","DDS"],
+        ["Passenger_Count","=","4"]
+    ],
+    "groupBy": [
+        "vendor_name",
+        "Passenger_Count"
+    ],
+    "aggregates": [
+        { "function": "SUM", "column": "Total_Amt" },
+        { "function": "SUM", "column": "Trip_Distance" },
+        { "function": "COUNT", "column": "*" },
+        { "function": "AVG", "column": "Total_Amt" }
+
+    ]
+}'
